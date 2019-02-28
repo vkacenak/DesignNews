f50858456166022f41127e30e36bfeb0179aba41
vkacenak <vkacenak@gmail.com>
2019-02-28 11:43:26 +0100

    progress

--- js/script.js
+++ js/script.js
@@ -6,6 +6,24 @@ function menuReveal() {
     document.getElementById("burger-line-2").classList.toggle("transition-burger-line-2");
     document.getElementById("burger-line-3").classList.toggle("transition-burger-line-3");
 }
+// SCROLL
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.querySelector(".desktop-nav").style.height = "0";
+    document.querySelector(".desktop-nav-articles").style.height = "7rem";
+    document.querySelector(".desktop-nav-articles").style.paddingTop = "2rem";
+
+   
+
+  } else {
+    document.querySelector(".desktop-nav").style.height = "10rem";
+    document.querySelector(".desktop-nav-articles").style.height = "5rem";
+    document.querySelector(".desktop-nav-articles").style.paddingTop = "0rem";
+   
+  }
+}
 
 const params = new URLSearchParams(location.search)
 var articleIds = params.get('id');
@@ -18,10 +36,20 @@ const pListLink = "";
 const articlesLink = "https://spreadsheets.google.com/feeds/list/1zV1J-ryAejwmZMSujMgOOb4eeO6ppNUm0lECjl495FU/od6/public/values?alt=json";
 function LoadJSON(){
 
-fetch(articlesLink).then(result => result.json()).then(article => article.feed.entry.forEach(displayData))
+//    fetch(articlesLink).then(result => result.json()).then(article =>  article.feed.entry.forEach(displayData));
+    fetch(articlesLink).then(result => result.json()).then(articles =>  sortData(articles.feed.entry));
+
+function sortData(articles){
+    console.log(articles);
+    var sortedArticles = articles.sort(function(a, b) {return b.gsx$popularity.$t - a.gsx$popularity.$t});
+    console.log(sortedArticles);
+    sortedArticles.forEach(displayData);
+}
 
 }
+var i = 0;
 function displayData(article) {
+i = i + 1;
     const clone = template.cloneNode(true);
     
  
@@ -31,7 +59,7 @@ function displayData(article) {
     clone.querySelector(".article__info").classList.add('.article__info-' + article.gsx$class.$t);
     clone.querySelector(".article__author").textContent = article.gsx$author.$t;
     clone.querySelector(".article__text").textContent = article.gsx$description.$t;
-//      clone.querySelector(".article__img").src = imgLink + "small/" + article.gsx$image.$t + "-sm.jpg";
+//      clone.querySelector(".article__img").src = + "img/" + article.gsx$image.$t;
 //     clone.querySelector(".article__img").alt = article.gsx$image.$t;
         // SETS ID FOR EVERY ARTICLE    
         let articleID = 'articleID-' + article.gsx$id.$t; 
@@ -40,8 +68,8 @@ function displayData(article) {
  function passID(){
     window.open('article.html'+'?id='+ article.gsx$id.$t,"");
 }
-
-    section.appendChild(clone);
+if (i < 4){
+    section.appendChild(clone);}
 }
 LoadJSON();
 
